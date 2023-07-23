2950acf37c7d189e3e0024e6e3c2116dbe2b5686
Alex <alexander.long@auroraer.com>
2023-07-23 19:25:47 +0100

    Add PalleteWeights class

--- rubiks/pallete_class.py
+++ rubiks/pallete_class.py
@@ -136,3 +136,79 @@ class Pallete(dict):
         # Check that all the values of the colour dict are Colours:
         for v in colour_dict.values():
             check_type(v, Colour, "a value in colour_dict")
+
+
+class PalleteWeights(dict):
+    """
+    Colour weights. Basically a dictionary.
+    """
+
+    def __init__(self, weights_dict: dict[str, float]) -> None:
+        """
+        Pallete weights. Basically a dictionary.
+
+        :param weights_dict: a dictionary where the keys are colour names and the values are float weights.
+        """
+        # Input validation:
+        PalleteWeights.__validate_weights_dict(weights_dict)
+
+        # Do regular dictionary stuff:
+        super().__init__(weights_dict)
+
+        # Also update things that need updating:
+        self.__update()
+
+    def __update(self) -> None:
+        """
+        Updates various attributes.
+        :return: None
+        """
+        # Weights and colour names:
+        self.names = list(self.keys())
+        self.weights = list(self.values())
+        # Combined as a dictionary:
+        self.weights_dict = dict(zip(self.names, self.weights))
+
+    def __setitem__(self, key: str, value: Colour) -> None:
+        """
+        Set self[key] to value.
+        :param key: a string key.
+        :param value: a float or int value.
+        :return: None
+        """
+        # Check that the key and value are valid inputs:
+        PalleteWeights.__validate_key_value_pair(key, value)
+
+        # Do normal dictionary things:
+        super().__setitem__(key, value)
+
+        # Also update things that need updating:
+        self.__update()
+
+    @staticmethod
+    def __validate_key_value_pair(key: Any, value: Any) -> None:
+        """
+        Check the given key, value pair is acceptable.
+        :param key: a string key.
+        :param value: a float or int value.
+        :return: None
+        """
+        # Check they are the correct types:
+        check_type(key, str, "a key")
+        check_type(value, [float, int], "a value")
+
+    @staticmethod
+    def __validate_weights_dict(weights_dict: Any) -> None:
+        """
+        Check the given weights dict is acceptable.
+        :param weights_dict: a dictionary mapping colour names to weights.
+        :return: None
+        """
+        # Check weights dict is a dictionary:
+        check_type(weights_dict, dict, "weights_dict")
+        # Check that the keys of the weights dict are strings:
+        for k in weights_dict.keys():
+            check_type(k, str, "a key in weights_dict")
+        # Check that all the values of the weights dict are floats or ints:
+        for v in weights_dict.values():
+            check_type(v, [float, int], "a value in weights_dict")
