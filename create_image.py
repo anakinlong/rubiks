4b557deecd22a222d9d945e56c141d42c859b886
anakin <alex@trevorder.com>
2023-07-28 23:12:35 +0100

    use classmethods for inheritance in Transformations

--- create_image.py
+++ create_image.py
@@ -1,26 +1,40 @@
+import cv2
 import rubiks
 
-a = 10
-rubiks.check_type(a, [str, list], "a")
 
 # The workflow I am envisioning:
 
 # Read in our image:
 # regular_image = cv2.imread(image_folder + image_file_name)
+image_folder = "Images\\"
+image_name = "bionicle.jpg"
+regular_image = cv2.imread(image_folder + image_name)
+# cv2.imshow(image_name, regular_image)
+# cv2.waitKey(0)
 
 # # Estimate the best dimensions for the final image:
 # (new_height, new_width) = rubiks.rubiks_dimension_estimator(regular_image, n_cubes, reporting=True)
+n_cubes = 400
+new_height, new_width = rubiks.rubiks_dimension_estimator(n_cubes, image=regular_image)
+print((new_height, new_width))
 
 # # Resize the image:
-# resized_regular_image = rubiks.resize_image(regular_image, (new_height, new_width))
+# resized_regular_image = rubiks.resize_image(regular_image, new_height=new_height, new_width=new_width)
+resized_regular_image = rubiks.resize_image(regular_image, new_height=new_height, new_width=new_width)
+# cv2.imshow(image_name, resized_regular_image)
+# cv2.waitKey(0)
 
 # # Do some transformations of the image:
-# e.g. do the simple "change each pixel to the nearest colour in the pallete":
-# rubiks_image = rubiks.RecolourClosest(pallete).image(resized_regular_image)
-# or "change each pixel to the colour in the pallete that has the most similar brightness":
-# rubiks_image = rubiks.RecolourGreyscale(pallete).image(resized_regular_image)
-# or "change each pixel to the nearest colour of the pallete, but also include combinations of those pallete colours":
-# rubiks_image = rubiks.RecolourClosestCombined(pallete).image(resized_regular_image)
+# e.g. do the simple "change each pixel to the nearest colour in the palette":
+# rubiks_image = rubiks.RecolourClosest.transform_image(resized_regular_image, palette)
+# or "change each pixel to the colour in the palette that has the most similar brightness:
+# rubiks_image = rubiks.RecolourGreyscale.transform_image(resized_regular_image, palette)
+# or "change each pixel to the nearest colour of the palette, but also include combinations of those palette colours":
+# rubiks_image = rubiks.RecolourClosestCombined.transform_image(resized_regular_image, combined_palette)
+palette = rubiks.RUBIKS_PALETTE
+rubiks_image = rubiks.RecolourClosest.transform_image(resized_regular_image, palette)
+cv2.imshow(image_name, rubiks.resize_image(rubiks_image, scale=10))
+cv2.waitKey(0)
 
 # # Scale the image up so we can see it, and show the two pixelated images side-by-side:
 # scale  = math.floor(image_scale * width / new_width)
--- rubiks/transformations.py
+++ rubiks/transformations.py
@@ -1,33 +1,30 @@
 """
-Rubiks image transformations
+Rubiks image transformations.
 """
 
+from abc import ABC
 import numpy as np
 import cv2
 from typing import Any, Callable
 from .palette_class import Palette, PaletteWeights
 
 
-class Transformation:
+class Transformation(ABC):
     """
     An abstract transformation. Lays out the general flow of actual transformations.
     """
 
-    def __init__(self, *args, **kwargs) -> None:
-        """
-        Initialize.
-        """
-        pass
-
-    def transform_image(self, image: cv2.Mat, *args, **kwargs) -> cv2.Mat:
+    @classmethod
+    def transform_image(cls, image: cv2.Mat, *args, **kwargs) -> cv2.Mat:
         """
         Main transform method for an image.
         :param image: an image.
         :return: a transformed image.
         """
-        return self.__apply_to_all_pixels(image, self.transform_pixel, *args, **kwargs)
+        return cls.__apply_to_all_pixels(image, cls.__transform_pixel, *args, **kwargs)
 
-    def transform_pixel(self, pixel: np.ndarray, *args, **kwargs) -> np.ndarray:
+    @classmethod
+    def __transform_pixel(cls, pixel: np.ndarray, *args, **kwargs) -> np.ndarray:
         """
         Main transform method for a pixel.
         :param pixel: a pixel.
@@ -35,8 +32,9 @@ class Transformation:
         """
         return pixel
 
+    @classmethod
     def __apply_to_all_pixels(
-        self,
+        cls,
         image: cv2.Mat,
         transformation: Callable[[np.ndarray, Any], np.ndarray],
         *args,
@@ -55,9 +53,9 @@ class Transformation:
                 # Find the current pixel:
                 original_pixel = image[y, x]
                 # Transform it:
-                transformed_image[y, x] = transformation(
-                    original_pixel, *args, **kwargs
-                )
+                transformed_image[y, x] = transformation(original_pixel, *args, **kwargs)
+
+        return transformed_image
 
 
 class RecolourClosest(Transformation):
@@ -65,61 +63,60 @@ class RecolourClosest(Transformation):
     Recolour each pixel to the colour in the palette that is closest geometrically.
     """
 
-    def __init__(self, palette: Palette, weights: PaletteWeights | None = None) -> None:
-        super().__init__()
-
-        # Input validation:
-        weights = self.__validate_weights(palette, weights)
-
-        # Store the palette and colour weights:
-        self.palette = palette
-        self.weights = weights
-
-    def transform_image(self, image: cv2.Mat) -> cv2.Mat:
-        return super().transform_image(image, self.palette, self.weights)
-
-    def transform_pixel(self, pixel: np.ndarray) -> np.ndarray:
-
-        return super().transform_pixel(pixel, self.palette, self.weights)
-
-    def __apply_to_all_pixels(
-        self,
+    @classmethod
+    def transform_image(
+        cls,
         image: cv2.Mat,
-        transformation: Callable[[np.ndarray, Palette], np.ndarray],
         palette: Palette,
-        weights: dict[str, float],
+        palette_weights: PaletteWeights | None = None,
     ) -> cv2.Mat:
-        return super().__apply_to_all_pixels(image, transformation, palette, weights)
+        """
+        Recolour each pixel of an image by chosing the colour in the palette which is geometrically closest.
+        :param image: an image in the form of a cv2.Mat.
+        :param palette: a palette of colours from which the final image will be constructed.
+        :param palette_weights: a map from colour names to "weights", which will determine how big of a sphere of
+        influence each colour has. Defaults to all colours having equal weights.
+        :return: an image (cv2.Mat) made up of the colours in the given palette.
+        """
+        # Input verification for the weights (if None, creates equal weight for each colour):
+        palette_weights = cls.__validate_weights(palette, palette_weights)
+
+        return super().transform_image(image, palette, palette_weights)
+
+    @classmethod
+    def __transform_pixel(
+        cls,
+        pixel: np.ndarray,
+        palette: Palette,
+        palette_weights: PaletteWeights | None = None,
+    ) -> np.ndarray:
+        # TODO implement
+        return super().__transform_pixel(pixel, palette, palette_weights)
 
     @staticmethod
-    def __validate_weights(
-        palette: Palette, weights: PaletteWeights | None
-    ) -> PaletteWeights:
-        """ """
+    def __validate_weights(palette: Palette, palette_weights: PaletteWeights | None) -> PaletteWeights:
+        """
+        Do input validation for the palette weights.
+        :param palette: a Palette of Colours.
+        :param pallete_weights: a PaletteWeights instance.
+        :return: palette_weights
+        """
         # If we haven't been given any weights, assign each colour a weight of 1:
-        if weights is None:
-            weights = PaletteWeights({colour_name: 1 for colour_name in palette.names})
+        if palette_weights is None:
+            palette_weights = PaletteWeights({colour_name: 1 for colour_name in palette.names})
 
         # Check that each colour in the palette has a weight:
         colours_with_no_weight = [
-            colour_name
-            for colour_name in palette.names
-            if colour_name not in weights.names
+            colour_name for colour_name in palette.names if colour_name not in palette_weights.names
         ]
         if colours_with_no_weight:
-            raise ValueError(
-                f"The following colours were not assigned weights:\n{colours_with_no_weight}"
-            )
+            raise ValueError(f"The following colours were not assigned weights:\n{colours_with_no_weight}")
 
         # Check that only colours from the palette are in the weights:
-        extra_colours = [
-            colour_name
-            for colour_name in weights.names
-            if colour_name not in palette.names
-        ]
+        extra_colours = [colour_name for colour_name in palette_weights.names if colour_name not in palette.names]
         if extra_colours:
             raise ValueError(
                 f"The following colours are not in the palette but were assigned weights:\n{extra_colours}"
             )
 
-        return weights
+        return palette_weights
