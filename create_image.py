719a362968b8a97b91ac40bc76d2a015574d7e56
anakin <alex@trevorder.com>
2023-07-29 22:00:48 +0100

    better flow of create_image

--- create_image.py
+++ create_image.py
@@ -9,8 +9,8 @@ import rubiks
 image_folder = "Images\\"
 image_name = "bionicle.jpg"
 regular_image = cv2.imread(image_folder + image_name)
-# cv2.imshow(image_name, regular_image)
-# cv2.waitKey(0)
+cv2.imshow(image_name, regular_image)
+cv2.waitKey(0)
 
 # # Estimate the best dimensions for the final image:
 # (new_height, new_width) = rubiks.rubiks_dimension_estimator(regular_image, n_cubes, reporting=True)
@@ -20,11 +20,13 @@ print((new_height, new_width))
 
 # # Resize the image:
 # resized_regular_image = rubiks.resize_image(regular_image, new_height=new_height, new_width=new_width)
-resized_regular_image = rubiks.resize_image(regular_image, new_height=new_height, new_width=new_width)
-# cv2.imshow(image_name, resized_regular_image)
-# cv2.waitKey(0)
+pixelated_regular_image = rubiks.resize_image(regular_image, new_height=new_height, new_width=new_width)
+# Show a scaled-up version of the pixelated image:
+scale = regular_image.shape[0] / new_height
+cv2.imshow(image_name, rubiks.resize_image(pixelated_regular_image, scale=scale))
+cv2.waitKey(0)
 
-# # Do some transformations of the image:
+# # Do some transformations of the pixelated image:
 # e.g. do the simple "change each pixel to the nearest colour in the palette":
 # rubiks_image = rubiks.RecolourClosest.transform_image(resized_regular_image, palette)
 # or "change each pixel to the colour in the palette that has the most similar brightness:
@@ -32,8 +34,8 @@ resized_regular_image = rubiks.resize_image(regular_image, new_height=new_height
 # or "change each pixel to the nearest colour of the palette, but also include combinations of those palette colours":
 # rubiks_image = rubiks.RecolourClosestCombined.transform_image(resized_regular_image, combined_palette)
 palette = rubiks.RUBIKS_PALETTE
-rubiks_image = rubiks.RecolourClosest.transform_image(resized_regular_image, palette)
-cv2.imshow(image_name, rubiks.resize_image(rubiks_image, scale=10))
+rubiks_image = rubiks.RecolourClosest.transform_image(pixelated_regular_image, palette)
+cv2.imshow(image_name, rubiks.resize_image(rubiks_image, scale=scale))
 cv2.waitKey(0)
 
 # # Scale the image up so we can see it, and show the two pixelated images side-by-side:
@@ -43,4 +45,4 @@ cv2.waitKey(0)
 # cv2.imshow("im", horizontal)
 # cv2.waitKey(0)
 
-# cv2.imwrite("Rubiks Images\mona lisa rbks.png", rubiks_image)
+# cv2.imwrite("Rubiks Images\bionicle rbks.png", rubiks_image)
