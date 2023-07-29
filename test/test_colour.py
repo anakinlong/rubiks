32585425cd113d5c1a2d3d1341821deb19de57af
anakin <alex@trevorder.com>
2023-07-29 03:29:07 +0100

    final Colour unit tests

--- test/test_colour.py
+++ test/test_colour.py
@@ -12,6 +12,12 @@ class ColourTest(unittest.TestCase):
 
         self.colour = Colour([1, 2, 3], channel_format=["b", "g", "r"])
 
+    def test_empty(self) -> None:
+        """
+        Test that supplying no arguments still produces a Colour.
+        """
+        self.assertIsInstance(Colour(), Colour)
+
     def test_values(self) -> None:
         """
         Test that the values property returns the expected result.
@@ -24,11 +30,25 @@ class ColourTest(unittest.TestCase):
         """
         self.assertEqual(self.colour.format, ["b", "g", "r"])
 
+    def test_setitem(self) -> None:
+        """
+        Test that list[index] = value (__setitem__) works on Colour.
+        """
+        self.colour[0] = 5
+        self.assertEqual(self.colour, Colour([5, 2, 3], channel_format=["b", "g", "r"]))
+
     def test_channel_value(self) -> None:
         """
         Test that the channel_value method returns the expected result.
         """
         self.assertEqual(self.colour.channel_value("b"), 1)
+
+    def test_channel_value_invalid_channel(self) -> None:
+        """
+        Test that the channel_value method raises a ValueError if the channel is not in the channel format.
+        """
+        with self.assertRaises(ValueError):
+            self.colour.channel_value("x")
 
     def test_reformat(self) -> None:
         """
@@ -40,6 +60,14 @@ class ColourTest(unittest.TestCase):
         # Create an equivalent colour:
         equivalent_colour = Colour([3, 2, 1], channel_format=["r", "g", "b"])
         self.assertEqual(self.colour, equivalent_colour)
+
+    def test_reformat_invalid_format(self) -> None:
+        """
+        Test that the reformat method raises a ValueError if the format is not a permutation of the current format.
+        """
+        with self.assertRaises(ValueError):
+            new_channel_format = ["x", "g", "b"]
+            self.colour.reformat(new_channel_format)
 
     def test_show(self) -> None:
         """
