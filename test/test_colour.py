d0314001be93b636dde99a97ebd456a5b36afbd5
anakin <alex@trevorder.com>
2023-07-29 02:55:03 +0100

    only import Colour when testing colour

--- test/test_colour.py
+++ test/test_colour.py
@@ -1,6 +1,6 @@
 import unittest
 
-import rubiks
+from rubiks import Colour
 
 
 class ColourTest(unittest.TestCase):
@@ -10,7 +10,7 @@ class ColourTest(unittest.TestCase):
 
     def setUp(self) -> None:
 
-        self.colour = rubiks.Colour([1, 2, 3], channel_format=["b", "g", "r"])
+        self.colour = Colour([1, 2, 3], channel_format=["b", "g", "r"])
 
     def test_values(self) -> None:
         """
@@ -38,7 +38,7 @@ class ColourTest(unittest.TestCase):
         new_channel_format = ["r", "g", "b"]
         self.colour.reformat(new_channel_format)
         # Create an equivalent colour:
-        equivalent_colour = rubiks.Colour([3, 2, 1], channel_format=["r", "g", "b"])
+        equivalent_colour = Colour([3, 2, 1], channel_format=["r", "g", "b"])
         self.assertEqual(self.colour, equivalent_colour)
 
     def test_show(self) -> None:
@@ -53,11 +53,11 @@ class ColourTest(unittest.TestCase):
         Test that the colour average method returns the expected result when not using weights.
         """
         # Calculate the average of these two colours:
-        colour_1 = rubiks.Colour([1, 2, 3], channel_format=["r", "g", "b"])
-        colour_2 = rubiks.Colour([10, 20, 30], channel_format=["g", "b", "r"])
-        colour_average = rubiks.Colour.colour_average(colour_1, colour_2)
+        colour_1 = Colour([1, 2, 3], channel_format=["r", "g", "b"])
+        colour_2 = Colour([10, 20, 30], channel_format=["g", "b", "r"])
+        colour_average = Colour.colour_average(colour_1, colour_2)
         # Create an equivalent colour:
-        equivalent_colour = rubiks.Colour([16, 6, 12], channel_format=["r", "g", "b"])
+        equivalent_colour = Colour([16, 6, 12], channel_format=["r", "g", "b"])
         self.assertEqual(colour_average, equivalent_colour)
 
     def test_colour_average_weighted(self) -> None:
@@ -65,12 +65,12 @@ class ColourTest(unittest.TestCase):
         Test that the colour average method returns the expected result when using weights.
         """
         # Calculate the average of these two colours:
-        colour_1 = rubiks.Colour([1, 2, 3], channel_format=["r", "g", "b"])
-        colour_2 = rubiks.Colour([10, 20, 30], channel_format=["g", "b", "r"])
+        colour_1 = Colour([1, 2, 3], channel_format=["r", "g", "b"])
+        colour_2 = Colour([10, 20, 30], channel_format=["g", "b", "r"])
         weights = [1, 2]
-        colour_average = rubiks.Colour.colour_average(colour_1, colour_2, weights=weights)
+        colour_average = Colour.colour_average(colour_1, colour_2, weights=weights)
         # Create an equivalent colour:
-        equivalent_colour = rubiks.Colour([20, 7, 14], channel_format=["r", "g", "b"])
+        equivalent_colour = Colour([20, 7, 14], channel_format=["r", "g", "b"])
         self.assertEqual(colour_average, equivalent_colour)
 
     def test_validate_iterable_iterable_type(self) -> None:
@@ -78,28 +78,28 @@ class ColourTest(unittest.TestCase):
         Test that the __validate_iterable method raises a TypeError when the iterable is not iterable.
         """
         with self.assertRaises(TypeError):
-            rubiks.Colour._Colour__validate_iterable(3)
+            Colour._Colour__validate_iterable(3)
 
     def test_validate_iterable_value(self) -> None:
         """
         Test that the __validate_iterable method raises a ValueError when the iterable does not have a length of 3.
         """
         with self.assertRaises(ValueError):
-            rubiks.Colour._Colour__validate_iterable([1, 2, 3, 4])
+            Colour._Colour__validate_iterable([1, 2, 3, 4])
 
     def test_validate_iterable_element_type(self) -> None:
         """
         Test that the __validate_iterable method raises a TypeError when the iterable does not contain integer elements.
         """
         with self.assertRaises(TypeError):
-            rubiks.Colour._Colour__validate_iterable(["1", "2", "3"])
+            Colour._Colour__validate_iterable(["1", "2", "3"])
 
     def test_validate_channel_format_channel_format_type(self) -> None:
         """
         Test that the __validate_channel_format method raises a TypeError when the channel_format is not a list.
         """
         with self.assertRaises(TypeError):
-            rubiks.Colour._Colour__validate_channel_format(3)
+            Colour._Colour__validate_channel_format(3)
 
     def test_validate_channel_format_value(self) -> None:
         """
@@ -107,7 +107,7 @@ class ColourTest(unittest.TestCase):
         length of 3.
         """
         with self.assertRaises(ValueError):
-            rubiks.Colour._Colour__validate_channel_format(["1", "2", "3", "4"])
+            Colour._Colour__validate_channel_format(["1", "2", "3", "4"])
 
     def test_validate_channel_format_element_type(self) -> None:
         """
@@ -115,7 +115,7 @@ class ColourTest(unittest.TestCase):
         string elements.
         """
         with self.assertRaises(TypeError):
-            rubiks.Colour._Colour__validate_channel_format([1, 2, 3])
+            Colour._Colour__validate_channel_format([1, 2, 3])
 
     def test_validate_channel_format_element_duplicates(self) -> None:
         """
@@ -123,4 +123,4 @@ class ColourTest(unittest.TestCase):
         duplicates.
         """
         with self.assertRaises(ValueError):
-            rubiks.Colour._Colour__validate_channel_format(["1", "1", "2"])
+            Colour._Colour__validate_channel_format(["1", "1", "2"])
