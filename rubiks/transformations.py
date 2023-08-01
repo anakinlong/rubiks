0d81974e48951165f089d353a6ecf8e80fb96437
anakin <alex@trevorder.com>
2023-08-01 23:00:36 +0100

    make greyscale transformation a child of recolour closest

--- rubiks/transformations.py
+++ rubiks/transformations.py
@@ -120,8 +120,8 @@ class RecolourClosest(Transformation):
 
         return recolour_closest_weighted(pixel, palette, palette_weights)
 
-    @staticmethod
-    def _validate_palette(palette: Palette) -> Palette:
+    @classmethod
+    def _validate_palette(cls, palette: Palette) -> Palette:
         """
         Do input validation for the palette.
 
@@ -134,8 +134,8 @@ class RecolourClosest(Transformation):
 
         return palette
 
-    @staticmethod
-    def _validate_weights(palette: Palette, palette_weights: PaletteWeights | None) -> PaletteWeights:
+    @classmethod
+    def _validate_weights(cls, palette: Palette, palette_weights: PaletteWeights | None) -> PaletteWeights:
         """
         Do input validation for the palette weights.
 
@@ -154,33 +154,10 @@ class RecolourClosest(Transformation):
         return palette_weights
 
 
-class RecolourClosestGreyscale(Transformation):
+class RecolourClosestGreyscale(RecolourClosest):
     """
     Recolour each pixel to the colour in the palette that is closest geometrically when converted to greyscale.
     """
-
-    @classmethod
-    def transform_image(
-        cls,
-        image: cv2.Mat,
-        palette: Palette,
-        palette_weights: PaletteWeights | None = None,
-    ) -> cv2.Mat:
-        """
-        Recolour each pixel of an image by chosing the colour in the palette which is geometrically closest when
-        converted to greyscale.
-
-        :param image: an image in the form of a cv2.Mat.
-        :param palette: a palette of colours from which the final image will be constructed.
-        :param palette_weights: a map from colour names to "weights", which will determine how big of a sphere of
-        influence each colour has. Defaults to all colours having equal weights.
-
-        :return: an image (cv2.Mat) made up of the colours in the given palette.
-        """
-        # Input verification for the weights (if None, creates equal weight for each colour):
-        palette_weights = cls._validate_weights(palette, palette_weights)
-
-        return super()._apply_to_all_pixels(image, palette, palette_weights)
 
     @classmethod
     def _transform_pixel(cls, pixel: np.ndarray, palette: Palette, palette_weights: PaletteWeights) -> np.ndarray:
@@ -198,22 +175,3 @@ class RecolourClosestGreyscale(Transformation):
         """
 
         return recolour_closest_weighted_greyscale(pixel, palette, palette_weights)
-
-    @staticmethod
-    def _validate_weights(palette: Palette, palette_weights: PaletteWeights | None) -> PaletteWeights:
-        """
-        Do input validation for the palette weights.
-
-        :param palette: a Palette of Colours.
-        :param pallete_weights: a PaletteWeights instance.
-
-        :return: palette_weights
-        """
-        # If we haven't been given any weights, assign each colour a weight of 1:
-        if palette_weights is None:
-            palette_weights = PaletteWeights({colour_name: 1 for colour_name in palette.names})
-
-        # Check that the palette weights and palette share the same colours:
-        palette_weights.validate_against_palette(palette)
-
-        return palette_weights
