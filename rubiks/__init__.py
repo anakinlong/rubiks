9852e8007943b3b3c048edaf368b0e21a6db343d
anakin <alex@trevorder.com>
2023-09-06 21:19:40 +0100

    import Pixel when importing the library

--- rubiks/__init__.py
+++ rubiks/__init__.py
@@ -3,5 +3,6 @@ from .constants import *
 from .methods import *
 from .colour_class import Colour
 from .palette_class import Palette, PaletteWeights
+from .pixel_class import Pixel
 from .pixel_transformations import recolour_closest_weighted, recolour_closest_weighted_greyscale
 from .transformations import Transformation, NoneTransformation, RecolourClosest, RecolourClosestGreyscale
