05234d62eb0c93102f71b547be328d79bb002a15
anakin <alex@trevorder.com>
2023-09-06 21:01:28 +0100

    subclassing ndarray is weird

--- rubiks/pixel_class.py
+++ rubiks/pixel_class.py
@@ -2,10 +2,10 @@
 Colour class for rubiks images.
 """
 
-from __future__ import annotations
 import numpy as np
 from typing import Any, Iterable
-from .lib import check_type
+
+from numpy._typing import NDArray
 
 
 class Pixel(np.ndarray):
@@ -13,68 +13,46 @@ class Pixel(np.ndarray):
     A pixel :^). This is basically a numpy.ndarray with a few extra features.
     """
 
-    def __init__(self, iterable: Iterable[int], coordinates: tuple[int | None] = (None, None)) -> None:
+    def __new__(cls, array: np.ndarray[int], coordinates: tuple[int | None] = (None, None)) -> None:
         """
         A pixel :^). This is basically a numpy.ndarray with a few extra features.
 
         :param iterable: an iterable with length three containing positive integer elements.
         :param coordinates: an iterable with length three containing positive integer elements.
         """
+        # np.ndarrays are weird to subclass. See https://numpy.org/doc/stable/user/basics.subclassing.html
         # Input validation:
-        Pixel.__validate_iterable(iterable)
+        Pixel.__validate_array(array)
 
-        # Do regular ndarray stuff:
-        super().__init__(iterable)
+        # Create an ndarray:
+        obj = np.asarray(array).view(cls)
 
         # Store the coordinates:
-        self._x, self._y = coordinates
+        obj.x, obj.y = coordinates
 
-    @property
-    def x(self) -> int:
-        """
-        The 0-indexed x coordinate of the pixel in its image.
-        """
-        return self._x
+        return obj
 
-    @property
-    def y(self) -> int:
+    def __array_finalize__(self, obj: np.ndarray[int]) -> None:
         """
-        The 0-indexed y coordinate of the pixel in its image.
+        Set the public attributes of the new ndarray.
         """
-        return self._y
-
-    def __setitem__(self, index: int, value: Any) -> None:
-        """
-        Set self[index] to value.
-
-        :param index: the index.
-        :param value: the value.
-
-        :return: None
-        """
-        # Do the regular ndarray __setitem__:
-        super().__setitem__(index, value)
-
-        # Check this is still a valid pixel:
-        Pixel.__validate_iterable(self)
+        self.x = getattr(obj, "x", None)
+        self.y = getattr(obj, "y", None)
 
     @staticmethod
-    def __validate_iterable(iterable: Iterable[int]) -> None:
+    def __validate_array(array: np.ndarray[int]) -> None:
         """
-        Check the given iterable is acceptable.
+        Check the given array is acceptable.
 
-        :param iterable: an iterable (supposedly).
+        :param array: an array (supposedly).
 
         :return: None
         """
         # Check the type is correct:
         try:
-            iter(iterable)
+            iter(array)
         except TypeError:
             raise
         # Check the length is correct:
-        if len(iterable) != 3:
-            raise ValueError(f"Iterable must have exactly 3 elements. Found {len(iterable)}.\n{iterable}")
-        # Check the types of the elements are correct:
-        for element in iterable:
-            check_type(element, int, "an element of the pixel iterable")
+        if len(array) != 3:
+            raise ValueError(f"Array must have exactly 3 elements. Found {len(array)}.\n{array}")
