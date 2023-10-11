b1393d532f7a07e6733d13b14a4877a10a9b1220
anakin <alex@trevorder.com>
2023-10-11 23:41:55 +0100

    add recolour closest combined transformation

--- rubiks/transformations.py
+++ rubiks/transformations.py
@@ -5,10 +5,10 @@ Rubiks image transformations.
 from abc import ABC, abstractmethod
 import numpy as np
 import cv2
-from .palette_class import Palette, PaletteWeights
+from .palette_class import Palette, PaletteWeights, CombinedPalette, CombinedPaletteWeights
 from .constants import CV2_CHANNEL_FORMAT
 from .pixel_class import Pixel
-from .pixel_transformations import recolour_closest, recolour_closest_greyscale
+from .pixel_transformations import recolour_closest, recolour_closest_greyscale, recolour_closest_combined
 
 
 class Transformation(ABC):
@@ -182,3 +182,103 @@ class RecolourClosestGreyscale(TransformationFromPalette):
         """
 
         return recolour_closest_greyscale(pixel, palette, palette_weights)
+
+
+class TransformationFromCombinedPalette(Transformation):
+    """
+    A transformation which takes a palette (and weights) as inputs, then creates the corresponding combined versions.
+    """
+
+    @classmethod
+    def transform_image(
+        cls,
+        image: cv2.Mat,
+        palette: Palette,
+        palette_weights: PaletteWeights | None = None,
+    ) -> cv2.Mat:
+        """
+        Recolour each pixel of an image using a palette and palette weights.
+
+        :param image: an image in the form of a cv2.Mat.
+        :param palette: a palette of colours from which the final image will be constructed.
+        :param palette_weights: a map from colour names to "weights", which will determine how big of a sphere of
+        influence each colour has. Defaults to all colours having equal weights.
+
+        :return: an image (cv2.Mat) made up of the colours in the given palette.
+        """
+        # Input verification for the palette:
+        palette = cls._validate_palette(palette)
+        # Input verification for the weights (if None, creates equal weight for each colour):
+        palette_weights = cls._validate_weights(palette, palette_weights)
+
+        # Create the combined versions of the palette and weights:
+        combined_palette = CombinedPalette(palette)
+        combined_palette_weights = CombinedPaletteWeights(palette_weights)
+
+        return cls._apply_to_all_pixels(image, palette, palette_weights, combined_palette, combined_palette_weights)
+
+    @staticmethod
+    def _validate_palette(palette: Palette) -> Palette:
+        """
+        Do input validation for the palette.
+
+        :param palette: a Palette of Colours.
+
+        :return: palette
+        """
+        # Reformat to the same channel format as cv2.Mat has:
+        palette.reformat(CV2_CHANNEL_FORMAT)
+
+        return palette
+
+    @staticmethod
+    def _validate_weights(palette: Palette, palette_weights: PaletteWeights | None) -> PaletteWeights:
+        """
+        Do input validation for the palette weights.
+
+        :param palette: a Palette of Colours.
+        :param pallete_weights: a PaletteWeights instance.
+
+        :return: palette_weights
+        """
+        # If we haven't been given any weights, assign each colour a weight of 1:
+        if palette_weights is None:
+            palette_weights = PaletteWeights({colour_name: 1 for colour_name in palette.names})
+
+        # Check that the palette weights and palette share the same colours:
+        palette_weights.validate_against_palette(palette)
+
+        return palette_weights
+
+
+class RecolourClosestCombined(TransformationFromCombinedPalette):
+    """
+    Recolour each pixel to the colour in the palette or combined palette that is closest geometrically.
+    """
+
+    @classmethod
+    def _transform_pixel(
+        cls,
+        pixel: Pixel,
+        palette: Palette,
+        palette_weights: PaletteWeights,
+        combined_palette: Palette,
+        combined_palette_weights: PaletteWeights,
+    ) -> np.ndarray:
+        """
+        Change the colour of the pixel to the one from the palette which is geometrically closest, also considering
+        combinations of colours. If a combination of two colours is closest, one of those two colours will be chosen
+        based on the position of the pixel within its image.
+
+        :param pixel: a pixel.
+        :param palette: a palette of colours from which the final image will be constructed. All colours must have the
+        same channel format as a cv2.Mat.
+        :param palette_weights: a map from colour names to "weights", which will determine how big of a sphere of
+        influence each colour has.
+        :param combined_palette: the corresponding combined palette.
+        :param combined_palette_weights: the corresponding combined palette weights.
+
+        :return: the transformed pixel.
+        """
+
+        return recolour_closest_combined(pixel, palette, palette_weights, combined_palette, combined_palette_weights)
