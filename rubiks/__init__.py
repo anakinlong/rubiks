73e518543b06f6d2754a6ba71dc9c0cda7260029
anakin <alex@trevorder.com>
2023-10-12 00:33:29 +0100

    import combined stuff

--- rubiks/__init__.py
+++ rubiks/__init__.py
@@ -2,7 +2,13 @@
 from .constants import *
 from .methods import *
 from .colour_class import Colour
-from .palette_class import Palette, PaletteWeights
+from .palette_class import Palette, PaletteWeights, CombinedPalette, CombinedPaletteWeights
 from .pixel_class import Pixel
-from .pixel_transformations import recolour_closest, recolour_closest_greyscale
-from .transformations import Transformation, NoneTransformation, RecolourClosest, RecolourClosestGreyscale
+from .pixel_transformations import recolour_closest, recolour_closest_greyscale, recolour_closest_combined
+from .transformations import (
+    Transformation,
+    NoneTransformation,
+    RecolourClosest,
+    RecolourClosestGreyscale,
+    RecolourClosestCombined,
+)
